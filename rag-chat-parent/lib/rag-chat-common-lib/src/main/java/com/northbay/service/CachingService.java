2a0e6ed06d499d4a08a9344f312e688b6d43de26
Marwan Althaher <marwan_althaher@hotmail.com>
2025-09-23 18:15:43 +0400

    fix caching issue , to evict cache by key

--- rag-chat-parent/lib/rag-chat-common-lib/src/main/java/com/northbay/service/CachingService.java
+++ rag-chat-parent/lib/rag-chat-common-lib/src/main/java/com/northbay/service/CachingService.java
@@ -3,6 +3,7 @@ package com.northbay.service;
 import java.util.function.Consumer;
 
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -29,6 +30,19 @@ public class CachingService {
 	 * @return
 	 */
 	private Consumer<? super String> evict() {
-		return cacheName -> cacheManager.getCache(cacheName).clear();
+		return cacheName -> evictByCacheName(cacheName);
+	}
+
+	public void evictByCacheName(String cacheName) {
+		cacheManager.getCache(cacheName).clear();
+	}
+	
+	
+	public void evictByCacheNameAndKeys(String cacheName, Object ...keys) {
+		cacheManager.getCache(cacheName).evict(new SimpleKey(keys));
+	}
+	
+	public void evictByCacheNameAndKey(String cacheName, Object key) {
+		cacheManager.getCache(cacheName).evict(new SimpleKey(key));
 	}
 }
